acc933a5ce352529a0f725b29f3982aba536fab9
NDianaV <di.spvi0@gmail.com>
2024-03-01 19:44:34 +0300

    Commit

---
+++ data.js
@@ -0,0 +1,63 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-panorama_135",
+      "name": "Panorama_135",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [],
+      "infoHotspots": [
+        {
+          "yaw": -2.2797936126496747,
+          "pitch": -0.23626788034176016,
+          "title": "Навес для йоги",
+          "text": "Text"
+        },
+        {
+          "yaw": -1.1614995538876265,
+          "pitch": 0.16908198294182597,
+          "title": "Тренажер 1",
+          "text": "Text"
+        },
+        {
+          "yaw": -0.07177189149616403,
+          "pitch": 0.28836399260192636,
+          "title": "Тренажер 2",
+          "text": "Text"
+        },
+        {
+          "yaw": 1.882736411621842,
+          "pitch": -0.11292679744507517,
+          "title": "Тренажер 3",
+          "text": "Text"
+        }
+      ]
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
